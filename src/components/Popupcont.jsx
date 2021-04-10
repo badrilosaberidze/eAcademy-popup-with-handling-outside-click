cc6455cd8ae4d24edaa14338db9e825b1e43def2
badrilosaberidze <losaberidzebadri@gmail.com>
2021-04-10 20:11:42 +0400

    popup

---
+++ src/components/Popupcont.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const Popupcont = () => {
+  const [popupStatus, setPopupStatus] = useState(false);
+  const popupRef = useRef(null);
+
+  const handleOnClick = (e) => {
+    if (
+      popupStatus &&
+      popupRef.current &&
+      !popupRef.current.contains(e.target)
+    ) {
+      setPopupStatus(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleOnClick);
+
+    return () => {
+      document.removeEventListener("click", handleOnClick);
+    };
+  });
+
+  return (
+    <div className="popup-container">
+      <button
+        onClick={() => {
+          setPopupStatus(true);
+        }}
+      >
+        Click For Pop-up
+      </button>
+
+      {popupStatus && (
+        <div className="Popup" ref={popupRef}>
+          <button
+            onClick={() => {
+              setPopupStatus(false);
+            }}
+          >
+            Close Popup
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Popupcont;
